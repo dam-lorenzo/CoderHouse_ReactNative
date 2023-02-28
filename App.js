1635219dc950f815f257f4a529a710ebf9b208c7
damian <dam.lorenzo@hotmail.com>
2023-02-28 19:09:52 -0300

    Update App.js entrega 5

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import InputField from './src/components/InputField';
+import ListItems from './src/components/ListItems';
+import ModalRemove from './src/components/ModalRemove';
+import Title from './src/components/Title';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+    const [ items, setItems ] = useState([])
+    const [ text, setText ] = useState('')
+    const [ modalStatus, setModalStatus ] = useState(false)
+    const [ selectedItem, setSelectedItem ] = useState(null)
+
+    const addItem = () => {
+        if (text !== '') {
+            setItems([
+                ...items,
+                {
+                    id: Date.now(),
+                    text: text
+                }
+            ])
+            setText('')
+        }
+    }
+
+    const selectItem = (item) => {
+        setSelectedItem(item)
+        changeModalStatus()
+    }
+
+    const changeModalStatus = () => {
+        setModalStatus(!modalStatus)
+    }
+
+    const removeItem = (toRemove) => {
+        setItems(
+            items.filter((item) => item.text !== toRemove )
+            )
+        changeModalStatus()
+    }
+
+    console.log(items)
+
+    return (
+        <View style={styles.appView}>
+            <Title />
+            <InputField textValue={text} textHandler={setText} pressHandler={addItem}/>
+            <ListItems items={items} itemAction={selectItem}/>
+            <ModalRemove
+                modalVisibility={modalStatus}
+                handleVisibility={changeModalStatus}
+                deleteAction={removeItem}
+                selectedItem={selectedItem}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appView: {
+        flex: 1,
+        backgroundColor: 'darkgrey',
+        alignItems: 'center',
+    },
 });
+
+export default App
