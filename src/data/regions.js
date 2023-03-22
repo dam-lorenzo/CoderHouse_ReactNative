09f3785e85863329f26e9d8afbd2b33cd84b5181
damian <dam.lorenzo@hotmail.com>
2023-03-21 22:19:08 -0300

    clase 10

---
+++ src/data/regions.js
@@ -0,0 +1,48 @@
+
+export const REGIONS = [
+    {
+        title: "cave",
+        id: 1,
+        color: '#f5428d'
+    },
+    {
+        title: "forest",
+        id: 2,
+        color: '#f54242'
+    },
+    {
+        title: "grassland",
+        id: 3,
+        color: '#f5a442'
+    },
+    {
+        title: "mountain",
+        id: 4,
+        color: '#f5d142'
+    },
+    {
+        title: "rare",
+        id: 5,
+        color: '#f5428d'
+    },
+    {
+        title: "rough-terrain",
+        id: 6,
+        color: '#f54242'
+    },
+    {
+        title: "sea",
+        id: 7,
+        color: '#f5a442'
+    },
+    {
+        title: "urban",
+        id: 8,
+        color: '#f5d142'
+    },
+    {
+        title: "waters-edge",
+        id: 9,
+        color: '#f5428d'
+    }
+]
---
+++ src/screens/CategoryPokemonsScreen.js
@@ -0,0 +1,30 @@
+import { FlatList } from 'react-native'
+import React from 'react'
+
+import { POKEMONS } from '../data/pokemons'
+import PokemonItem from '../components/PokemonItem'
+
+const CategoryPokemonsScreen = ({ route, navigation }) => {
+
+    const { regionId } = route.params
+
+    const pokemons = POKEMONS.find(region => region.id === regionId)
+    
+    const handleOnSelected = (item) => {
+        navigation.navigate('Detail', {
+        bread: item
+        })
+    }
+
+    const renderPokemonItem = ({ item }) => (<PokemonItem item={item} onSelected={handleOnSelected} />)
+
+    return (
+        <FlatList 
+            data={pokemons.pokemon_species}
+            keyExtractor={(item) => item.id}
+            renderItem={renderPokemonItem}
+        />
+    )
+}
+
+export default CategoryPokemonsScreen
